f68a626284a7a71b83b97bf89789cc997cd0392c
yaroslaviy <yaselokeev@gmail.com>
2019-10-03 15:27:39 -0400

    introducing search

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {robots} from './robots.js';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		};
+	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	}
+	render() {
+		const filterRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		return (<div className='tc'>
+		<h1>RoboFriends</h1>
+		<SearchBox searchChange={this.onSearchChange}/>
+		<CardList robots={filterRobots}/>
+		</div>
+		);
+	}
+	
+}
+
+export default App;
---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+	const cardComponent = robots.map((user, i) => {
+		return 	<Card key={i} id={user.id} name={user.name} email={user.email}/>
+
+	})
+	return (
+		<div>
+			{cardComponent}
+		</div>
+		);
+}
+
+export default CardList;
